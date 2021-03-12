e295498662982e0cf3dfd61605050a2162858761
Ryan Downing <ryansdowning1@gmail.com>
2021-03-12 00:57:08 -0500

    more progress

--- smarter/base/home.py
+++ smarter/base/home.py
@@ -1,4 +1,6 @@
 import cv2
+from smarter.config import HUE_BRIDGE
+from phue import Bridge
 
 class Home:
     def __init__(self, rooms: list[Room], users: list[User]):
@@ -6,9 +8,10 @@ class Home:
 
 
 class Room:
-    def __init__(self, name: str, camera: Camera):
+    def __init__(self, name: str, devices: Dict[str, Device]):
         self.name = name
-        self.camera = camera
+        self.devices = devices
+        self.camera = devices['camera']
 
 
 class User:
@@ -16,9 +19,31 @@ class User:
         self.name = name
 
 
-class Camera:
-    def __init__(self, video: str):
+class Device:
+    def __init__(self, id_: str):
+        self.id = id_
+
+
+class Camera(Device):
+    def __init__(self, id_: str, video: str):
+        super(Camera, self).__init__(id_)
         self.video = cv2.VideoCapture(video)
 
     def detect_faces(self) -> list:
         raise NotImplementedError
+
+    def __str__(self):
+        return f"Camera Device: {self.id_}"
+
+
+class Light(Device):
+    def __init__(self, id_: str, light_id: int):
+        super(Camera, self).__init__(id_)
+        self.light_id = light_id
+        self.name = HUE_BRIDGE.get_light(self.light_id, 'name')
+
+    def set_light(self, *args, **kwargs):
+        return HUE_BRIDGE.set_light(self.light_id, *args, **kwargs)
+
+    def __str__(self):
+        return f"Light Device: {self.id_} with Light id: {self.light_id}"
--- smarter/config.py
+++ smarter/config.py
@@ -1,9 +1,18 @@
-ROOMS = {
-    'room1': 'ip1',
-    'room2': 'ip2',
-    'room3': 'ip3',
-    'room4', 'ip4',
+USERS = ('user1', 'user2', 'user3', 'user4')
+ROOMS = ('room1', 'room2', 'room3', 'room4')
+
+DEVICES = {
+    'camera1': {'room': 'room1', 'ip': 'ip1'},
+    'camera2': {'room': 'room2', 'ip': 'ip2'},
+    'camera3': {'room': 'room3', 'ip': 'ip3'},
+    'camera4': {'room': 'room4', 'ip': 'ip4'},
 }
 
-USERS = ('user1', 'user2', 'user3', 'user4')
+HUE_BRIDGE_IP = '192.168.'
+if HUE_BRIDGE_IP is not None:
+    from phue import Bridge
+    HUE_BRIDGE = Bridge(HUE_BRIDGE_IP)
+    HUE_BRIDGE.connect()
+else:
+    HUE_BRIDGE = None
 
